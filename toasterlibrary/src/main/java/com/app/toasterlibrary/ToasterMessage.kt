e862ba22339a5b23723532e3d6a35be1c21dbbd4
PriyankaK2022 <Priyanka.kale@carnot.co.in>
2022-10-03 21:01:46 +0530

    toast long

--- toasterlibrary/src/main/java/com/app/toasterlibrary/ToasterMessage.kt
+++ toasterlibrary/src/main/java/com/app/toasterlibrary/ToasterMessage.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 
 class ToasterMessage {
     companion object {
-        fun a(c: Context,message:String)  { Toast.makeText(c,message,Toast.LENGTH_SHORT).show();}
+        fun a(c: Context,message:String)  {
+            Toast.makeText(c,message,Toast.LENGTH_LONG).show();}
     }
 }
